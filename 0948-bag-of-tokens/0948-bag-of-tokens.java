7492491070935e03da2a95487e4737f64a197f6a
Abir Bhowmick <124705686+007abir@users.noreply.github.com>
2024-03-04 07:00:14 +0530

    Time: 2 ms (97.13%), Space: 42.5 MB (73.25%) - LeetHub

---
+++ 0948-bag-of-tokens/0948-bag-of-tokens.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int bagOfTokensScore(int[] tokens, int power) {
+        Arrays.sort(tokens);
+        int l = 0, r = tokens.length - 1;
+        int cur_score = 0, mx_score = 0;
+        while (l <= r) {
+            if (tokens[l] <= power) {
+                power -= tokens[l];
+                cur_score += 1;
+                mx_score = Math.max(mx_score, cur_score);
+                l += 1;
+            }
+            else if (cur_score >= 1) {
+                power += tokens[r];
+                cur_score -= 1;
+                r -= 1;
+            }
+            else
+                break;
+        }
+        return mx_score;
+    }
+}
