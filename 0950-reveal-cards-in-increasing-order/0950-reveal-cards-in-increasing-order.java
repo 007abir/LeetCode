d0f70a07d2755d64467bfcf23603f848717f1c96
Abir Bhowmick <124705686+007abir@users.noreply.github.com>
2024-04-10 06:17:06 +0530

    Time: 3 ms (97.57%), Space: 43.2 MB (86.41%) - LeetHub

---
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[] deckRevealedIncreasing(int[] deck) {
+        if(deck.length==1)
+            return deck;
+        
+        Arrays.sort(deck);
+        int res[] = new int[deck.length];
+        int k = 1;
+        int c = 0;
+        res[0] = deck[0];
+        
+        // insert the elements from the sorted array into every 2nd empty slot of result array
+        while(k<deck.length)
+        {
+           for(int i=1;i<deck.length;i++)
+           {               
+                if(res[i]==0)
+                {
+                    c++;
+                    if(c==2)
+                    {
+                        res[i] = deck[k++];
+                        c=0;
+                    }
+                }
+            }
+        }
+
+        return res;
+    }
+}
