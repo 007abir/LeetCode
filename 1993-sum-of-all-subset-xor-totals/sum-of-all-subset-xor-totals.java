e7d951d0ce59b495bf7296bf3ac1d1f191528a1c
Abir Bhowmick <124705686+007abir@users.noreply.github.com>
2024-05-20 06:19:44 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (91.14%) - LeetSync

---
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int subsetXORSum(int[] nums) {
+        int sumTotal=0;
+        for (int num:nums) {
+            sumTotal|=num;
+        }
+        return sumTotal<<(nums.length-1);
+    }
+}
